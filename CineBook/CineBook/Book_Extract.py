74e6a08baf1e028e9f3ef828fe1a90507ae1e692
astrocoolin <5259436+astrocoolin@users.noreply.github.com>
2020-06-02 10:08:49 -0400

    Changing project name, commiting first real code

---
+++ CineBook/CineBook/Book_Extract.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import MultiLabelBinarizer
+
+def get_book_labels(filename,no_features):
+    #temporarily working with an html file
+    f = open(filename)
+    html_soup = BeautifulSoup(f, 'html.parser')
+    Title = html_soup.title.text[:-12]
+    review_containers = html_soup.find_all('div',class_ ='review')
+    for i, review in enumerate(review_containers):
+        review_containers[i] = review.find('span', class_ = 'readable').text
+
+    CV = CountVectorizer(max_features=no_features, stop_words='english')
+    Book_CV = CV.fit_transform(review_containers)
+    Book_feature_names = CV.get_feature_names()
+    book_labels = Book_feature_names
+    return Title, book_labels
---
+++ CineBook/CineBook/Cinebook.py
@@ -0,0 +1,23 @@
+import matplotlib
+from Book_Extract import get_book_labels
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+
+matplotlib.use('tkAgg')
+
+no_features = 15
+book_titles,book_labels = get_book_labels('Book_page.html',no_features)
+df = pd.read_pickle('Moviedb.pkl')
+#df['Keywords'] = df['Keywords'].str.replace("'",'')
+
+One_book = pd.DataFrame({'Name':[book_titles], 'Keywords':[book_labels]})
+df2 =One_book.append(df).reset_index(drop=True)
+df =One_book.append(df).reset_index(drop=True)
+#df = df.append(One_book)
+mlb = MultiLabelBinarizer()
+df1 = df.join(pd.DataFrame(mlb.fit_transform(df.pop('Keywords')),
+                          columns=mlb.classes_,
+                          index=df.index))
+#print(book_labels)
+#print(movie_labels)
