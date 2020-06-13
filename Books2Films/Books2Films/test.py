88654bc3951e4bbfd8d99db9a346a5fa55646695
astrocoolin <25259436+astrocoolin@users.noreply.github.com>
2020-06-13 15:43:09 -0400

    cleaning up unused files

--- Books2Films/Books2Films/test.py
+++
@@ -1,5 +0,0 @@
-from DblPlusBooks import BookProcessing, Movie_Scrape
-nofeatures = 25
-book=Book('harry potter and the order of the phoenix',nofeatures)
-movies = Movies(book,nofeatures)
-H = movies.H
